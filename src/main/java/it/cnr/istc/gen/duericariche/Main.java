66a4f7012c89adb757081915ca0e90c0afb8e466
DA <assuntadauria@gmail.com>
2021-11-24 11:32:29 +0100

    Auto stash before merge of "master" and "origin/master"

--- src/main/java/it/cnr/istc/gen/duericariche/Main.java
+++ src/main/java/it/cnr/istc/gen/duericariche/Main.java
@@ -17,12 +17,16 @@ public class Main {
             id++;
         }
 
-        double H = 30; //altezza del foglio
-        double W = 50; //larghezza del foglio
+
+     
+
+        double H = 127; //altezza del foglio
+        double W = 203; //larghezza del foglio
+
         double areaFoglio = H * W; //area del foglio
 
-        double alpha = 3.2d; //costo spreco
-        double beta = 2.1d; //costo penalità
+        double alpha = 450; //costo spreco
+        double beta = 50; //costo penalità
 
 //        Ordine[] ordini = new Ordine[30];
 
@@ -38,38 +42,19 @@ public class Main {
 //        ordini[8] = new Ordine(2d, 2d, 3d, 3L, 3d);
 
         //TO BE CONTINUED..
-        /* Calcoliamo il vettore dei tempi di processamento in modo casuale
-		 
-		int [ ] tp = new int [10]; //vettore tempi di processamento
-		for (int i=0; i<10; i++) {
-			tp[i] = ((int) ((Math.random()*100)%2)+1); } //genero un vettore di lunghezza 10 con valori tra 1 e 2
-		
-         */
-        double[] tp = {0.5, 0.7, 1, 1.5, 2, 2.5, 3.1, 3.5};
-        double[] w = {1, 1.5, 1, 2, 1.5, 1.5, 1.5, 1.5};
-
-        /*Definiamo un vettore per peso di ogni job
-		double [] w = new double [10];
-		int w1=0;
-		for (int i=0; i<10; i++) {
-			w1=(int) ((Math.random()*100%3));
-			switch (w1) {
-			case 0: w[i]=1;
-					break;
-			case 1: w[i]=1.5;
-					break;
-			case 2: w[i]=2;
-					break;
-			}
-		}*/
-//Creiamo l'oggetto ga
+      
+// Creiamo l'oggetto ga
         GeneticAlgorithm ga = new GeneticAlgorithm(100, 0.01, 0.95, 0);
 
 //Inizializziamo la popolazione specificando la lunghezza dei cromosomi
-        Population population = ga.initPopulation(8);
+        Population population = ga.initPopulation(30);
 
 //Valutiamo la popolazione
-    OHNO    ga.evalPopulation(population, tp, w, alpha, MTBF, tg, tr);
+
+   
+
+       ga.evalPopulation(population, areaFoglio, alpha, beta, ordini) ;
+
 
         int generation = 1;
 
@@ -95,19 +80,18 @@ public class Main {
         System.out.println("Stopped after " + maxGenerations + " generations.");
         {
             Individual fittest = population.getFittest(0);
-            System.out.println("La fitness � " + fittest.getFitness());
+            System.out.println("La fitness è: " + fittest.getFitness());
         }
 
-        System.out.println("La schedulazione �: " + population.getFittest(0));
+        System.out.println("La schedulazione è: " + population.getFittest(0));
 
-   OHNO     double tempodicompletamento = ga.tempodicompletamento(population, tp, tr);
-   OHNO     System.out.println("Cmax �: " + tempodicompletamento);
 
-        System.out.print("tp �: ");
+
+      /*  System.out.print("tp �: ");
         for (int i = 0; i < 8; i++) {
             System.out.print(tp[i] + " ");
         }
-
+*/
     }
 
 }
